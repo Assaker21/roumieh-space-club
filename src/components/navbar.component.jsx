c66f7daff90e5f369ee7a933fc969dce50e71512
Assaker21 <charbelassaker@live.com>
2023-10-22 12:09:39 +0300

    second commit

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -1,17 +1,71 @@
+import { useEffect, useState } from "react";
 import "./navbar.component.scss";
 
 function Navbar() {
+  const [scroll, setScroll] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [openNavbar, setOpenNavbar] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY);
+    });
+
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth);
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {
+        setScroll(window.scrollY);
+      });
+
+      window.removeEventListener("resize", () => {
+        setWidth(window.innerWidth);
+      });
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
-      <h1 className="title">Roumieh Space Club</h1>
-      <ul className="menu-links">
-        <li className="menu-link">Home</li>
-        <li className="menu-link">Events</li>
-        <li className="menu-link">About</li>
-        <li className="menu-link">Articles</li>
-        <button className="join-button">Join us</button>
-      </ul>
-    </nav>
+    <>
+      <nav className={"navbar" + (scroll == 0 || openNavbar ? " start" : "")}>
+        <h1 className="title">Roumieh Space Club</h1>
+
+        {width > 700 && (
+          <ul className="menu-links">
+            <li className="menu-link">Home</li>
+            <li className="menu-link">Events</li>
+            <li className="menu-link">About</li>
+            <li className="menu-link">Articles</li>
+            <button className="join-button">Join us</button>
+          </ul>
+        )}
+
+        {width <= 700 && (
+          <div className="menu-links">
+            <button
+              className="ham-button"
+              onClick={() => {
+                setOpenNavbar(!openNavbar);
+              }}
+            >
+              <i className={openNavbar ? "bx bx-x" : "bx bx-menu"}></i>
+            </button>
+          </div>
+        )}
+      </nav>
+
+      <nav className={"narrow-navbar" + (openNavbar ? "" : " hidden")}>
+        <ul className="menu-links">
+          <li className="menu-link">Home</li>
+          <li className="menu-link">Events</li>
+          <li className="menu-link">About</li>
+          <li className="menu-link">Articles</li>
+          <button className="join-button">Join us</button>
+        </ul>
+      </nav>
+      <div className="navbar-offset"></div>
+    </>
   );
 }
 
--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,20 +1,54 @@
 import "./home.page.scss";
+import ReactPlayer from "react-player";
+import Draggable from "react-draggable";
 
 function Home() {
   return (
-    <section className="section">
-      <div className="landing-section-container">
-        <img src="/images/astronaut cartoon 1024.png" alt="" className="landing-section-image" />
-        <div className="landing-section-quote-container">
-          <h2 className="landing-section-quote">
-            &quot;Not only do we live among the stars,
-            <br /> the stars live within us.&quot;
-          </h2>
-          <p className="landing-section-quote-writer">~ Neil Degrasse Tyson</p>
+    <div className="home">
+      <section className="section landing-section">
+        <div className="landing-section-container">
+          <Draggable
+            onStart={() => {
+              window.addEventListener(
+                "touchmove",
+                (e) => {
+                  e.preventDefault();
+                },
+                { passive: false }
+              );
+            }}
+            onStop={() => {
+              window.removeEventListener(
+                "touchmove",
+                (e) => {
+                  e.preventDefault();
+                },
+                { passive: false }
+              );
+            }}
+          >
+            <img src="/images/astronaut cartoon 1024.png" alt="" className="landing-section-image" />
+          </Draggable>
+          <div className="landing-section-quote-container">
+            <h2 className="landing-section-quote">Learn, improve and have fun with the Roumieh Space Club</h2>
+            <p className="landing-section-quote-writer">We gather all space enthusiasts for all kinds of astronomical events and discussions. The club aims to spread its knowledge to the biggest audience possible.</p>
+            <button className="join-button">Join now - it's free!</button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className="section">
+        <div className="past-work-section-container">
+          <div className="section-title">Our past work</div>
+          <div className="video-container">
+            <ReactPlayer width={"100%"} height={"100%"} className="video" controls={true} url="https://player.vimeo.com/video/876799084?badge=0&amp;autopause=0&amp;quality_selector=1&amp;progress_bar=1&amp;player_id=0&amp;app_id=58479" />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
+
+//&quot;Not only do we live among the stars, the stars live within us.&quot;
+//~ Neil Degrasse Tyson
 
 export default Home;
