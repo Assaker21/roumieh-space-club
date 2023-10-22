abf1c40e673074df73fa73951522a0ed1a755f91
Assaker21 <charbelassaker@live.com>
2023-10-22 23:35:28 +0300

    add: meet the board and activities

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -3,9 +3,51 @@ import ReactPlayer from "react-player";
 import Draggable from "react-draggable";
 
 function Home() {
+  const boardMembers = [
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    },
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    },
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    },
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    },
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    },
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    },
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    },
+    {
+      name: "Charbel Assaker",
+      role: "Web developer",
+      image: "/images/charbel-assaker.jpg"
+    }
+  ];
+
   function preventDefault(e) {
     e.preventDefault();
-    console.log("here too");
   }
 
   return (
@@ -25,15 +67,72 @@ function Home() {
           <div className="landing-section-quote-container">
             <h2 className="landing-section-quote">Learn, improve and have fun with the Roumieh Space Club</h2>
             <p className="landing-section-quote-writer">We gather all space enthusiasts for all kinds of astronomical events and discussions. The club aims to spread its knowledge to the biggest audience possible.</p>
-            <button className="join-button">Join now - it's free!</button>
+            <button className="join-button">Join now - it&apos;s free!</button>
           </div>
         </div>
       </section>
+
       <section className="section">
-        <div className="past-work-section-container">
-          <div className="section-title">Our past work</div>
+        <div className="section-container">
+          <div className="section-title">Past activities</div>
           <div className="video-container">
-            <ReactPlayer width={"100%"} height={"100%"} className="video" controls={true} url="https://player.vimeo.com/video/876799084?badge=0&amp;autopause=0&amp;quality_selector=1&amp;progress_bar=1&amp;player_id=0&amp;app_id=58479" />
+            <ReactPlayer width={"100%"} height={"100%"} className="video" controls={true} url="https://player.vimeo.com/video/876834803?h=ac7ed2219a" />
+          </div>
+          <div className="past-activities-container">
+            <div className="past-activity-container">
+              <div className="past-activity-info">
+                <div className="past-activity-name">Stargazing night</div>
+                <div className="past-activity-location">Jbeil</div>
+                <div className="past-activity-date">21 09 2002</div>
+                <div className="past-activity-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, numquam molestiae consectetur esse debitis voluptate incidunt quaerat vel laudantium. Magni explicabo adipisci sit at dolorem quas perspiciatis asperiores eligendi culpa!</div>
+              </div>
+              <div className="past-activity-images">
+                <img src="/images/event.jpeg" alt="" className="past-activity-image" />
+              </div>
+            </div>
+
+            <div className="past-activity-container">
+              <div className="past-activity-images">
+                <img src="/images/event.jpeg" alt="" className="past-activity-image" />
+              </div>
+              <div className="past-activity-info">
+                <div className="past-activity-name">Stargazing night</div>
+                <div className="past-activity-location">Jbeil</div>
+                <div className="past-activity-date">21 09 2002</div>
+                <div className="past-activity-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, numquam molestiae consectetur esse debitis voluptate incidunt quaerat vel laudantium. Magni explicabo adipisci sit at dolorem quas perspiciatis asperiores eligendi culpa!</div>
+              </div>
+            </div>
+
+            <div className="past-activity-container">
+              <div className="past-activity-info">
+                <div className="past-activity-name">Stargazing night</div>
+                <div className="past-activity-location">Jbeil</div>
+                <div className="past-activity-date">21 09 2002</div>
+                <div className="past-activity-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, numquam molestiae consectetur esse debitis voluptate incidunt quaerat vel laudantium. Magni explicabo adipisci sit at dolorem quas perspiciatis asperiores eligendi culpa!</div>
+              </div>
+              <div className="past-activity-images">
+                <img src="/images/event.jpeg" alt="" className="past-activity-image" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <div className="section-container">
+          <div className="section-title">Meet the board</div>
+          <div className="board-members">
+            {boardMembers.map((boardMember) => {
+              return (
+                <div key={"Board member " + boardMember.name} className="board-member">
+                  <img src={boardMember.image} alt="" className="board-member-image" />
+                  <div className="board-member-info">
+                    <span className="board-member-name">{boardMember.name}</span>
+                    <span className="board-member-role">{boardMember.role}</span>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </section>
